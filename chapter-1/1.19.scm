72bc14e0f591d23465a70be6e7d24239dcc2bdd2
Vicky Chijwani <vickychijwani@gmail.com>
2011-12-10 17:21:28 +0530

    add 1.19.

---
+++ chapter-1/1.19.scm
@@ -0,0 +1,18 @@
+;;; p' and q' can be computed to:
+;;; p' = p^2 + q^2, q' = 2pq + q^2
+
+(define (fib n)
+  (define (fib-iter a b p q count)
+    (cond ((= count 0) b)
+          ((even? count)
+           (fib-iter a
+                     b
+                     (+ (* p p) (* q q))   ; p'
+                     (+ (* 2 p q) (* q q)) ; q'
+                     (/ count 2)))
+          (else (fib-iter (+ (* b q) (* a q) (* a p))
+                          (+ (* b p) (* a q))
+                          p
+                          q
+                          (- count 1)))))
+  (fib-iter 1 0 0 1 n))
