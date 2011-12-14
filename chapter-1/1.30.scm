f39f58e6d40a0a6e264ed6375a69364308a6e07f
Vicky Chijwani <vickychijwani@gmail.com>
2011-12-14 18:30:13 +0530

    add 1.30.

---
+++ chapter-1/1.30.scm
@@ -0,0 +1,8 @@
+;;; iterative higher order procedure for sum
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
