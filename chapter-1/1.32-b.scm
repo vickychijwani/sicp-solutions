88bb36f25f4d6daf2f68765d758760597390be36
Vicky Chijwani <vickychijwani@gmail.com>
2011-12-14 18:30:59 +0530

    add 1.32-b.

---
+++ chapter-1/1.32-b.scm
@@ -0,0 +1,8 @@
+;;; iterative higher order procedure for accumulation
+
+(define (accumulate combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
